dd92e8ba21e024e3997412cc9e40343a321df6e5
ArmanFahradyan <armanfahradyan02@gmail.com>
2023-03-31 12:18:50 +0400

    some changes

--- models/matching.py
+++ models/matching.py
@@ -61,12 +61,12 @@ class Matching(torch.nn.Module):
           data: dictionary with minimal keys: ['image0', 'image1']
         """
         pred = {}
-        pred['keypoints0'] = []
-        pred['scores0'] = []
-        pred['descriptors0'] = []
-        pred['keypoints1'] = []
-        pred['scores1'] = []
-        pred['descriptors1'] = []
+        pred['keypoints0'] = torch.empty((len(layers_of_first_img), 1), device='cuda')
+        pred['scores0'] = torch.empty((1, len(layers_of_first_img)), device='cuda')
+        pred['descriptors0'] = torch.empty((len(layers_of_first_img), 1), device='cuda')
+        pred['keypoints1'] = torch.empty((len(layers_of_second_img), 1), device='cuda')
+        pred['scores1'] = torch.empty((1, len(layers_of_second_img)), device='cuda')
+        pred['descriptors1'] = torch.empty((len(layers_of_second_img), 1), device='cuda')
 
         for a_layer in layers_of_first_img:
             a_layer = np.array(a_layer, np.uint8)
@@ -107,8 +107,11 @@ class Matching(torch.nn.Module):
                 for keypoint in tensor:
                     keypoint[0] = keypoint[0] + x_min_x
                     keypoint[1] = keypoint[1] + y_max_y
-                    pred['keypoints0'].append(keypoint)
+                pred['keypoints0'] = torch.cat((pred['keypoints0'], tensor), dim=0)
             
+            print('keyp', pred['keypoints0'])
+            input()
+
             for el in pred0['scores']:
                 for sc in el:
                     pred['scores0'].append(sc)
@@ -124,7 +127,7 @@ class Matching(torch.nn.Module):
                         pred['descriptors0'].append(tens) 
 
         # print('descriptors0', pred['descriptors0'])
-        # input()
+        input()
 
         for b_layer in layers_of_second_img:
             b_layer = np.array(b_layer, np.uint8)
@@ -167,13 +170,17 @@ class Matching(torch.nn.Module):
                     pred['scores1'].append(sc)
 
             for descr in pred1['descriptors']:
-                if descr.shape != torch.Size([256, 1]):
-                    descr = torch.split(descr, 1, dim=1)
-                    for i, tens in enumerate(descr):
-                        if tens.shape != torch.Size([256, 0]):
-                            pred['descriptors1'].append(tens)
-                else:
-                    pred['descriptors1'].append(tens) 
+                if descr.shape != torch.Size([256, 1]) and descr.shape != torch.Size([256, 0]):
+                    pred['descriptors1'].append(descr) 
+
+            # print('keyp', torch.tensor(pred['keypoints1']))
+            # input()
+                    # descr = torch.split(descr, 1, dim=1)
+                #     for i, tens in enumerate(descr):
+                #         if tens.shape != torch.Size([256, 0]):
+                #             pred['descriptors1'].append(tens)
+                # else:
+                #     pred['descriptors1'].append(tens) 
                 
 
         print('pred keypoints1', len(pred['keypoints1']))
@@ -188,7 +195,7 @@ class Matching(torch.nn.Module):
         # print('data', data.keys())
         for k in data:
             if isinstance(data[k], (list, tuple)):
-                data[k] = torch.stack(data[k])
+                data[k] = torch.stack(data[k], dim=0)
                  
         # data['scores0'] = torch.unsqueeze(data['scores0'], dim=1)
         # data['scores0'] = torch.unsqueeze(data['scores0'], dim=1)
